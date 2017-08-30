c5b5ec3dc66109847f006939874cebebfe63780e
Vincent <vboulaye@gmail.com>
2017-08-30 16:32:49 +0200

    affichage laplace aller/retour

--- ecran-ratp/js/main.js
+++ ecran-ratp/js/main.js
@@ -4,10 +4,11 @@ $(function () {
 
     var getData = function () {
 
-        var schedules_url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/arcueil+cachan/A';
+        var schedules_url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/laplace/A';
+        var schedules_r_url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/rers/b/laplace/R';
         var traffic_url = 'https://api-ratp.pierre-grimaud.fr/v3/traffic/rers/b';
 
-        $.when($.getJSON(schedules_url), $.getJSON(traffic_url)).done(function (schedules, traffic) {
+        $.when($.getJSON(schedules_url),$.getJSON(schedules_r_url), $.getJSON(traffic_url)).done(function (schedules,schedules_r,traffic) {
             var date = new Date(),
                 hours = date.getHours(),
                 minutes = date.getMinutes(),
@@ -15,15 +16,21 @@ $(function () {
 
             var trafficResponse = traffic[0].result,
                 scheduleResponse = schedules[0].result,
-                scheduleResponseInformation = scheduleResponse;
+                scheduleResponseInformation = scheduleResponse.schedules[0];
+
+            var filterMessages=function filterMessages(schedule){
+              return !schedule.message.match(/^Sans /);
+            };
 
             var data = {
                 traffic: trafficResponse.message,
                 line: 'B',
                 type: 'rer',
-                horaires: scheduleResponse.schedules,
-                destination: scheduleResponseInformation.destination,
-                station: 'Arcueil Cachan',
+                horaires: scheduleResponse.schedules.filter(filterMessages),
+                destination: scheduleResponseInformation.destination.split(/\. */),
+                horaires_r: schedules_r[0].result.schedules.filter(filterMessages),
+                destination_r: schedules_r[0].result.schedules[0].destination.split(/\. */),
+                station: 'Laplace',
                 current_time: current_time
             };
 
